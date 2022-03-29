31b3c85af1547705e40b67582b6a198d3bb2167c
ajswebdesigns <andrew@ajswebdesigns.com>
2022-03-29 19:48:18 -0400

    more

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended:true}))
 
+item = '';
 app.get('/', (req, res)=>{
   let today = new Date();
   let options = {
@@ -13,11 +15,13 @@ app.get('/', (req, res)=>{
   };
   let day = today.toLocaleDateString('en-US', options)
 
-  res.render('list', {kindOfDay: day})
+  res.render('list', {kindOfDay: day, newListItem: item});
 })
 
-
-
+app.post('/', (req, res)=>{
+item = req.body.newItem
+  res.redirect('/')
+})
 
 app.listen(3000, ()=>{
   console.log('Server started on port 3000')
