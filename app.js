277c98355462e32195ae89a8fa2dbdb02f6b55e8
ajswebdesigns <andrew@ajswebdesigns.com>
2022-03-30 12:43:04 -0400

    done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('public'))
 
 items = []
+workItems = []
 app.get('/', (req, res)=>{
   let today = new Date();
   let options = {
@@ -16,15 +17,33 @@ app.get('/', (req, res)=>{
   };
   let day = today.toLocaleDateString('en-US', options)
 
-  res.render('list', {kindOfDay: day, newListItems: items});
+  res.render('list', {listTitle: day, newListItems: items});
 })
 
 app.post('/', (req, res)=>{
-let item = req.body.newItem
-items.push(item)
-  res.redirect('/')
+  let item = req.body.newItem
+  if(req.body.list === 'Work'){
+    workItems.push(item)
+    res.redirect('/work')
+  } else {
+    items.push(item)
+    res.redirect('/')
+  }
 })
 
+app.get('/work', (req, res)=>{
+  res.render('list', {listTitle: 'Work List:', newListItems: workItems})
+})
+app.post('/work', (req, res)=>{
+  let item = req.body.newItem
+  workItems.push(item)
+  res.redirect('/work')
+})
+
+
+app.get('/about',(req, res)=>{
+  res.render('about')
+})
 app.listen(3000, ()=>{
   console.log('Server started on port 3000')
 });
