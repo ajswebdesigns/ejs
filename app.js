d4ea63cd8c3ec6280d60639c100e5e15226580e8
ajswebdesigns <andrew@ajswebdesigns.com>
2022-03-30 14:11:29 -0400

    more modules

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const date = require(__dirname + '/date.js')
 const app = express()
+
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended:true}))
@@ -9,14 +11,7 @@ app.use(express.static('public'))
 items = []
 workItems = []
 app.get('/', (req, res)=>{
-  let today = new Date();
-  let options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long'
-  };
-  let day = today.toLocaleDateString('en-US', options)
-
+  let day = date()
   res.render('list', {listTitle: day, newListItems: items});
 })
 
